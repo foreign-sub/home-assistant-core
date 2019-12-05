ac57141feb91ac5ec8b2821a614f9ef9d0045a8e
Restyled.io <commits@restyled.io>
2019-12-05 18:53:05 +0000

    Restyled by yapf

--- homeassistant/requirements.py
+++ homeassistant/requirements.py
@@ -27,14 +27,16 @@ class RequirementsNotFound(HomeAssistantError):
 
     def __init__(self, domain: str, requirements: List) -> None:
         """Initialize a component not found error."""
-        super().__init__(f"Requirements for {domain} not found: {requirements}.")
+        super().__init__(
+            f"Requirements for {domain} not found: {requirements}.")
         self.domain = domain
         self.requirements = requirements
 
 
-async def async_get_integration_with_requirements(
-    hass: HomeAssistant, domain: str, done: Set[str] = None
-) -> Integration:
+async def async_get_integration_with_requirements(hass: HomeAssistant,
+                                                  domain: str,
+                                                  done: Set[str] = None
+                                                  ) -> Integration:
     """Get an integration with installed requirements.
 
     This can raise IntegrationNotFound if manifest or integration
@@ -52,30 +54,25 @@ async def async_get_integration_with_requirements(
         return integration
 
     if integration.requirements:
-        await async_process_requirements(
-            hass, integration.domain, integration.requirements
-        )
+        await async_process_requirements(hass, integration.domain,
+                                         integration.requirements)
 
     deps_to_check = [
-        dep
-        for dep in integration.dependencies + (integration.after_dependencies or [])
-        if dep not in done
+        dep for dep in integration.dependencies +
+        (integration.after_dependencies or []) if dep not in done
     ]
 
     if deps_to_check:
-        await asyncio.gather(
-            *[
-                async_get_integration_with_requirements(hass, dep, done)
-                for dep in deps_to_check
-            ]
-        )
+        await asyncio.gather(*[
+            async_get_integration_with_requirements(hass, dep, done)
+            for dep in deps_to_check
+        ])
 
     return integration
 
 
-async def async_process_requirements(
-    hass: HomeAssistant, name: str, requirements: List[str]
-) -> None:
+async def async_process_requirements(hass: HomeAssistant, name: str,
+                                     requirements: List[str]) -> None:
     """Install the requirements for a component or platform.
 
     This method is a coroutine. It will raise RequirementsNotFound
@@ -92,7 +89,8 @@ async def async_process_requirements(
             if pkg_util.is_installed(req):
                 continue
 
-            ret = await hass.async_add_executor_job(_install, hass, req, kwargs)
+            ret = await hass.async_add_executor_job(_install, hass, req,
+                                                    kwargs)
 
             if not ret:
                 raise RequirementsNotFound(name, [req])
@@ -112,7 +110,8 @@ def pip_kwargs(config_dir: Optional[str]) -> Dict[str, Any]:
     """Return keyword arguments for PIP install."""
     is_docker = pkg_util.is_docker_env()
     kwargs = {
-        "constraints": os.path.join(os.path.dirname(__file__), CONSTRAINT_FILE),
+        "constraints": os.path.join(os.path.dirname(__file__),
+                                    CONSTRAINT_FILE),
         "no_cache_dir": is_docker,
     }
     if "WHEELS_LINKS" in os.environ:
