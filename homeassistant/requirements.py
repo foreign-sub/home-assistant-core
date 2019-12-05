cd5e90597908c6faaf11dc5db8ae3bfef3f1f972
Restyled.io <commits@restyled.io>
2019-12-05 18:52:57 +0000

    Restyled by reorder-python-imports

--- homeassistant/requirements.py
+++ homeassistant/requirements.py
@@ -1,14 +1,19 @@
 """Module to handle installing requirements."""
 import asyncio
-from pathlib import Path
 import logging
 import os
-from typing import Any, Dict, List, Optional, Set
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Set
 
-from homeassistant.exceptions import HomeAssistantError
 import homeassistant.util.package as pkg_util
 from homeassistant.core import HomeAssistant
-from homeassistant.loader import async_get_integration, Integration
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.loader import async_get_integration
+from homeassistant.loader import Integration
 
 DATA_PIP_LOCK = "pip_lock"
 DATA_PKG_CACHE = "pkg_cache"
