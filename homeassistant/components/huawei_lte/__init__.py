c1d74b449b07c0ce07718409fc0d6d50de281e94
Restyled.io <commits@restyled.io>
2019-12-28 12:20:26 +0000

    Restyled by black

--- homeassistant/components/huawei_lte/__init__.py
+++ homeassistant/components/huawei_lte/__init__.py
@@ -180,7 +180,7 @@ class Router:
                 _LOGGER.debug("Trying to authorize again...")
                 if self.connection.enforce_authorized_connection():
                     _LOGGER.debug(
-                        "...success, %s will be updated by a future periodic run", key,
+                        "...success, %s will be updated by a future periodic run", key
                     )
                 else:
                     _LOGGER.debug("...failed")
@@ -480,7 +480,7 @@ async def async_setup(hass: HomeAssistantType, config) -> bool:
 
     for service in ADMIN_SERVICES:
         hass.helpers.service.async_register_admin_service(
-            DOMAIN, service, service_handler, schema=SERVICE_SCHEMA,
+            DOMAIN, service, service_handler, schema=SERVICE_SCHEMA
         )
 
     for url, router_config in domain_config.items():
